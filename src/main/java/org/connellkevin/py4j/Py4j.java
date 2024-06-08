6b3081b3a0df117c82e927df9a91cbbfb81fe313
Connell Boyce <connellboyce17@gmail.com>
2024-06-08 13:59:43 -0400

    feat: Initialize project

---
+++ src/main/java/org/connellkevin/py4j/Py4j.java
@@ -0,0 +1,7 @@
+package org.connellkevin.py4j;
+
+public class Py4j {
+    public int execute() {
+        return 0;
+    }
+}
---
+++ src/test/java/org/connellkevin/py4j/Py4jTest.java
@@ -0,0 +1,13 @@
+package org.connellkevin.py4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Py4jTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
